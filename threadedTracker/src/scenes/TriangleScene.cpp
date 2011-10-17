99af625a25b6bc0ed6cdd61383ba187fa37a62d6
zach lieberman <zach@eyebeam.org>
2011-10-17 17:29:45 +0200

    nothing major

---
+++ threadedTracker/src/scenes/TriangleScene.cpp
@@ -0,0 +1,16 @@
+
+
+#include "triangleScene.h"
+
+
+void triangleScene::update(ofxFaceTracker & tracker){
+	//
+//	if (tracker.getFound()){
+//			imageMesh = tracker.
+//	}
+}
+
+void triangleScene::draw(ofTexture & cam){
+	
+}
+	
--- threadedTracker/src/scenes/TriangleScene.h
+++ threadedTracker/src/scenes/TriangleScene.h
@@ -1,30 +1,23 @@
 #pragma once
 
-#include "NoiseParticle.h"
+#include "ofMain.h"
+#include "meanMeshRenderer.h"
+#include "ofxFaceTracker.h"
 
-// the particle system is based on 0,0 as the center of the screen
-// so you might need to apply an offset when sending the mouth position
 
-class RibbonsScene {
-public:
-	void setup() {
-	}
-	void update(bool mouthIsOpen, ofVec2f mouthPosition = ofVec2f()) {
-		if(mouthIsOpen) {
-			NoiseParticle::setLeaving(true);
-			NoiseParticle::setup(10, ofVec2f(mouthPosition.x - ofGetWidth() / 2, mouthPosition.y - ofGetHeight() / 2), 1);
-			NoiseParticle::addForce(ofVec2f(mouthPosition.x - ofGetWidth() / 2, mouthPosition.y - ofGetHeight() / 2), 3);
-		}
-		NoiseParticle::updateAll(60 * ofGetLastFrameTime());
-	}
-	void draw() {
-		ofPushMatrix();
-		ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2, 0);
+
+class triangleScene {
+
+	public:
+	
 		
-		ofEnableSmoothing();
-		ofEnableBlendMode(OF_BLENDMODE_ADD);
-		ofSetLineWidth(4);
-		NoiseParticle::drawAllLines();
-		ofPopMatrix();
-	}
+		void update(ofxFaceTracker & tracker);
+		void draw(ofTexture & cam); 
+	
+		float overallEnergy;
+		ofMesh imageMesh; 
+		ofFbo temp;
+		
+		
+	
 };
