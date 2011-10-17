4d08e7db036e3dfc4d3a16c54b1d266f340778f0
Kyle McDonald <kyle@kylemcdonald.net>
2011-10-16 19:58:14 -0500

    simplified and expanded

--- Mandala/src/ofApp.cpp
+++ Mandala/src/ofApp.cpp
@@ -2,31 +2,32 @@
 
 float scale = 4;
 		
-void drawRing(ofPoint pos, float rad, bool bInward, float startAngle, int ndivisions) {
+void drawTriangleRing(float radius, float theta, float height, bool direction, int count) {
+	radius *= scale / 4;
 	float angle = 0;
 	float angle2;
-	float radius = (rad/4) * scale;
-	float ndiv = ndivisions;
-	float ndiv_m_1 = ndiv - 1;
-	for (int i = 0; i < (ndivisions); i++){
-		angle = startAngle + ((float)i/(ndiv)) * TWO_PI;
-		angle2 = angle + ((float)1.0/(ndiv)) * TWO_PI;
+	height *= (sqrt(3) / 2) * (direction ? -1 : 1);
+	for (int i = 0; i < (count); i++){
+		angle = theta + (i * TWO_PI) / count;
+		angle2 = angle + TWO_PI / count;
 		ofPoint ptA, ptB, ptC;
 		float angle3 = (angle+angle2)/2.0;
-		ptA.set(pos.x + radius * cos(angle), pos.y + radius * sin(angle));
-		ptB.set(pos.x + radius * cos(angle2), pos.y + radius * sin(angle2));
+		ptA.set(radius * cos(angle), radius * sin(angle));
+		ptB.set(radius * cos(angle2), radius * sin(angle2));
 		float dist = (ptA-ptB).length();
-		ptC.set(pos.x + (radius + (bInward ? -1 : 1) * (sqrt(3)/2.0)*dist) * cos(angle3), 
-				pos.y + (radius +  (bInward ? -1 : 1) * (sqrt(3)/2.0)*dist) * sin(angle3));
+		ptC.set((radius + height * dist) * cos(angle3), 
+				(radius +  height * dist) * sin(angle3));
 		ofTriangle(ptA, ptB, ptC);
 	}
 }
 
 void drawMandala(ofVec2f position, float radius) {
-	ofSetColor(255, 48);
-	drawRing(position, 130, false, ofGetElapsedTimef()/2, 40);
-	drawRing(position, 128, true, -ofGetElapsedTimef()/2, 40);
-	drawRing(position, 110, true, -(ofGetElapsedTimef()/3.0), 60);
+	ofSetColor(255, 128);
+	ofTranslate(position.x, position.y);
+	drawTriangleRing(130, -(ofGetElapsedTimef()/8.0), 8, false, 80);
+	drawTriangleRing(130, ofGetElapsedTimef()/2, 1, false, 40);
+	drawTriangleRing(128, -ofGetElapsedTimef()/2, 1, true, 40);
+	drawTriangleRing(110, -(ofGetElapsedTimef()/3.0), 1, true, 60);
 }
 
 void ofApp::setup() {
